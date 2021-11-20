30cd5f65ada2ce62f2aa64fd6e12aba530c64517
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-11-20 13:53:54 +0900

    fix: モデル修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    binding.pry
     if @user.save
-      redirect_to ""
+      redirect_to root_path
     else
       render :new
     end
@@ -15,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confimation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_120_032_315) do
+ActiveRecord::Schema.define(version: 2021_11_20_032315) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,4 +24,5 @@ ActiveRecord::Schema.define(version: 20_211_120_032_315) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
