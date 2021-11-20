43ec7a4352cdcb9e85ccc51f55af649ad0ea20b0
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-11-20 13:20:49 +0900

    add: pry-byebugをインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'boards#top'
+
+  resources :users, only: [:new, :create]
   resources :boards
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
