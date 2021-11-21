30d175c1910c53db4dec09b65942f07a0e6935e1
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-11-21 15:02:22 +0900

    fix: モデルにバリデーション付与

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,6 @@
 class Board < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { minimum: 5, maximum: 15 }
+  validates :body, presence: true, length: { minimum: 10, maximum: 200 }
 end
