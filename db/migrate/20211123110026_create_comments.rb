f62bf3785b43dd12154c961fe5e5d7222daec979
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-11-23 20:04:12 +0900

    add: rails db:migrateを実行

--- db/migrate/20211123110026_create_comments.rb
+++ db/migrate/20211123110026_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.string :body
-      t.references :user, null: false, foreign_key: true
+      t.string :body,      null: false
+      t.references :user,  null: false, foreign_key: true
       t.references :board, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_121_023_408) do
+ActiveRecord::Schema.define(version: 2021_11_23_110026) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +28,16 @@ ActiveRecord::Schema.define(version: 20_211_121_023_408) do
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "body", null: false
+    t.bigint "user_id", null: false
+    t.bigint "board_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_comments_on_board_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -38,4 +49,6 @@ ActiveRecord::Schema.define(version: 20_211_121_023_408) do
   end
 
   add_foreign_key "boards", "users"
+  add_foreign_key "comments", "boards"
+  add_foreign_key "comments", "users"
 end
