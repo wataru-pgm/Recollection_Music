46558a229f57bbdd9955ef53b833bba2c50bb9aa
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-11-23 21:34:40 +0900

    add: コメント入力フォームの作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
+    redirect_to board_comments_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(board_id: params[:board_id])
+  end
 end
