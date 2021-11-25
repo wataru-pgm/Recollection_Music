6e39db8e21e7af6a7296f3c90bc08d3b0ac5c45c
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-11-25 21:32:43 +0900

    ass: yarnでfontawsomeをインストール

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,15 @@
 class LikesController < ApplicationController
   def create
+    @like = Like.new(user_id: currnet_user.id, board_id: @board.id)
+    @likes.save
   end
 
   def destroy
+  end
+
+  private
+
+  def set_board
+    @board = Board.find_by(id: params[:board_id])
   end
 end
