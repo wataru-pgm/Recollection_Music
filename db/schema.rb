4a2c25b8a76a44390680b686f35370f6e33c6f61
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-11-26 23:28:22 +0900

    fix: カラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_132609) do
+ActiveRecord::Schema.define(version: 2021_11_26_142723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_11_26_132609) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
