aebafb4ddec7e9cbf9c2373e75fefb708e3a96a7
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-11-26 21:15:41 +0900

    fix: scssのfontawsomeの読み込みを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap';
-import '../stylesheets/application';
 import '@fortawesome/fontawesome-free/js/all';
+import '../stylesheets/application';
 
 require('jquery')
 
