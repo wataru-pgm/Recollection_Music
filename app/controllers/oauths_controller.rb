3f1eb68d3e2994698cbc46411518605646e4c764
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-12-06 00:30:35 +0900

    fix: コントローラ周り修正

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -2,7 +2,7 @@ class OauthsController < ApplicationController
   def oauth
     login_at(params[:provider])
   end
-      
+
   def callback
     provider = params[:provider]
     if @user = login_from(provider)
@@ -10,9 +10,8 @@ class OauthsController < ApplicationController
     else
       begin
         @user = create_from(provider)
-        # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
 
-        reset_session # protect from session fixation attack
+        reset_session
         auto_login(@user)
         redirect_to root_path, :notice => "Logged in from #{provider.titleize}!"
       rescue
