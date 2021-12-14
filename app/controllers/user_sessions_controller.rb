e58ab8419fca3e6292c16d7ea31f066a0b83e59b
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-12-14 21:08:49 +0900

    add: アクセス制限の付与

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,11 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login
   def new; end
 
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, notice: 'ログインしました！'
+      redirect_back_or_to boards_path, notice: 'ログインしました！'
     else
       render :new
     end
