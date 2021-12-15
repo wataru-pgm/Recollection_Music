472f22ef284f11c585c2d024911133f4d768609c
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-12-15 22:44:14 +0900

    fix: rails db:migrate:resetを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_126_142_723) do
+ActiveRecord::Schema.define(version: 2021_11_26_142723) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +44,7 @@ ActiveRecord::Schema.define(version: 20_211_126_142_723) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["board_id"], name: "index_likes_on_board_id"
-    t.index %w[user_id board_id], name: "index_likes_on_user_id_and_board_id", unique: true
+    t.index ["user_id", "board_id"], name: "index_likes_on_user_id_and_board_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
@@ -54,7 +55,6 @@ ActiveRecord::Schema.define(version: 20_211_126_142_723) do
     t.string "salt"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
     t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
