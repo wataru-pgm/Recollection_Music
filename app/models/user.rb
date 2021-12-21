6ad42c287ff22074c20cc272d5cd74ac03c11fbc
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-12-21 21:03:46 +0900

    add: アソシエーションの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :like_boards, through: :likes, source: :board
 
+  has_many :relationships, dependent: :destroy
+  has_many :followings, through: relationships, source: :follower
+
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followers, through: :relationships, source: :user
+
   validates :name, presence: true, length: { minimum: 2, maximum: 15 }
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
