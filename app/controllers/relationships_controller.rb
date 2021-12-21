45da8cef9384e70589f1652ec507529350e87617
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-12-21 22:19:02 +0900

    add: relationships_controllerにコメント追記

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,17 @@
 class RelationshipsController < ApplicationController
+  # フォローする
   def create
+    # paramsでフォロワーを取得
     @other_user = User.find(params[:follower])
+    # ログインしているユーザが@other_userをフォローする
     current_user.follow(@other_user)
   end
 
+  # フォロー外す
   def destroy
+    # ログインしているユーザの情報をrelationshipsテーブルのfollower_idを取得する
     @user = current_user.relationships.find(params[:id]).follower
+    # ログインしているユーザがフォローを外す
     current_user.unfollow(params[:id])
   end
 end
