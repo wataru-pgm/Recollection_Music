af0313304c1ba0199b5cb0ab157762ee94bc8bbb
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-01-03 11:48:49 +0900

    add: sorceryファイルの作成

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -118,7 +118,7 @@ Rails.application.config.sorcery.configure do |config|
   config.twitter.key = Rails.application.credentials.dig(:twitter, :key)
   config.twitter.secret = Rails.application.credentials.dig(:twitter, :secret_key)
   config.twitter.callback_url = "http://localhost:3000/oauth/callback?provider=twitter"
-  config.twitter.user_info_mapping = {:email => "screen_name"}
+  config.twitter.user_info_mapping = {email: "screen_name", name: "name"}
   #
   # config.facebook.key = ""
   # config.facebook.secret = ""
@@ -532,7 +532,7 @@ Rails.application.config.sorcery.configure do |config|
     # Class which holds the various external provider data for this user.
     # Default: `nil`
     #
-    # user.authentications_class =
+    user.authentications_class = Authentication
 
     # User's identifier in the `authentications` class.
     # Default: `:user_id`
