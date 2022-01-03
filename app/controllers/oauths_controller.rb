3b000e91ca61236c6d1e4df001187fe17fe5a5b4
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-01-03 12:05:09 +0900

    add: oauthsコントローラを作成

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,7 +1,22 @@
 class OauthsController < ApplicationController
   def oauth
+    login_at(params[:provider])
   end
 
   def callback
+    provider = params[:provider]
+    if @user = login_from(provider)
+      redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+    else
+      begin
+        @user = create_from(provider)
+
+        reset_session
+        auto_login(@user)
+        redirect_to root_path, notice:"#{provider.titleize}でログインしました"
+      rescue
+        redirect_to root_path, alert:"#{provider.titleize}でのログインに失敗しました"
+      end
+    end
   end
 end
