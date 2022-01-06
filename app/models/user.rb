9f1818ef6724f1b7496a53d8489a9090e3c475be
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-01-06 23:15:49 +0900

    fix: モデル（フォロワー）の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follower
   # フォローされている
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
-  has_many :followers, through: :relationships, source: :user
+  has_many :followers, through: :passive_relationships, source: :user
 
   validates :name, presence: true, length: { minimum: 2, maximum: 15 }
   validates :email, presence: true, uniqueness: true
