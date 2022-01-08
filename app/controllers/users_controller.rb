0a312c1803f6df120f4700e93fe5092082837fe5
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-01-08 18:18:06 +0900

    add: 新規登録後のオートログインを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      auto_login(@user)
+      redirect_to boards_path
     else
       render :new
     end
