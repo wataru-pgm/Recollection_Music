77a31c01858d50dcfcd5e2c7bf844dcef07c8c87
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-01-09 23:05:29 +0900

    fix: コード修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_02_135055) do
-
+ActiveRecord::Schema.define(version: 20_220_102_135_055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +20,7 @@ ActiveRecord::Schema.define(version: 2022_01_02_135055) do
     t.string "uid", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
+    t.index %w[provider uid], name: "index_authentications_on_provider_and_uid"
   end
 
   create_table "boards", force: :cascade do |t|
@@ -53,7 +52,7 @@ ActiveRecord::Schema.define(version: 2022_01_02_135055) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["board_id"], name: "index_likes_on_board_id"
-    t.index ["user_id", "board_id"], name: "index_likes_on_user_id_and_board_id", unique: true
+    t.index %w[user_id board_id], name: "index_likes_on_user_id_and_board_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
@@ -63,7 +62,7 @@ ActiveRecord::Schema.define(version: 2022_01_02_135055) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
-    t.index ["user_id", "follower_id"], name: "index_relationships_on_user_id_and_follower_id", unique: true
+    t.index %w[user_id follower_id], name: "index_relationships_on_user_id_and_follower_id", unique: true
     t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
