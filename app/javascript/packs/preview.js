86caa2e0c42cf9791944d029021d9efafc2dc2ee
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-01-16 23:44:13 +0900

    add: 画像のアップロードの表示

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,5 +1,20 @@
 if (document.URL.match(/edit/)){
   document.addEventListener('DOMContentLoaded', () => {
+    const createImageHTML = (blob) => {
+      const imageElement = document.getElementById('new-image');
 
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('class', 'new-img')
+      blobImage.setAttribute('src', blob);
+
+      imageElement.appendChild(blobImage);
+    };
+
+
+    document.getElementById('profile-image').addEventListener('change', (e) => {
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
+    });
   });
 }
