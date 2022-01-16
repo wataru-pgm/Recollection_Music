fd8890375a99c6777f99515913e57196a1c93522
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-01-17 01:06:07 +0900

    fix: editで画像が表示されるように修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,20 +1,18 @@
-if (document.URL.match(/edit/)){
-  document.addEventListener('DOMContentLoaded', () => {
-    const createImageHTML = (blob) => {
-      const imageElement = document.getElementById('new-image');
 
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('class', 'new-img')
-      blobImage.setAttribute('src', blob);
+document.addEventListener('DOMContentLoaded', () => {
+  const createImageHTML = (blob) => {
+    const imageElement = document.getElementById('new-image');
 
-      imageElement.appendChild(blobImage);
-    };
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('class', 'new-img')
+    blobImage.setAttribute('src', blob);
 
+    imageElement.appendChild(blobImage);
+  };
 
-    document.getElementById('profile-image').addEventListener('change', (e) => {
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-      createImageHTML(blob);
-    });
+  document.getElementById('profile-image').addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    createImageHTML(blob);
   });
-}
+});
