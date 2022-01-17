e6b91d82d27ad20b4e355ac8b3d393f49d1a83f5
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-01-17 20:26:01 +0900

    fix: コード修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,18 +1,20 @@
+if (document.URL.match(/edit/)){
+  document.addEventListener('DOMContentLoaded', () => {
+    const createImageHTML = (blob) => {
+      const imageElement = document.getElementById('new-image');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const createImageHTML = (blob) => {
-    const imageElement = document.getElementById('new-image');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('class', 'new-img')
+      blobImage.setAttribute('src', blob);
 
-    const blobImage = document.createElement('img');
-    blobImage.setAttribute('class', 'new-img')
-    blobImage.setAttribute('src', blob);
+      imageElement.appendChild(blobImage);
+    };
 
-    imageElement.appendChild(blobImage);
-  };
 
-  document.getElementById('profile-image').addEventListener('change', (e) => {
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
-    createImageHTML(blob);
+    document.getElementById('profile-image').addEventListener('change', (e) => {
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
+    });
   });
-});
+}
