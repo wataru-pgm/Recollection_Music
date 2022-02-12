556d9867ae205eb4cd580cbe06279277e4bd1a41
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-02-12 23:20:30 +0900

    fix: フラッシュメッセージの文言修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -22,8 +22,9 @@ class BoardsController < ApplicationController
   def create
     @board = current_user.boards.new(board_params)
     if @board.save
-      redirect_to boards_path
+      redirect_to boards_path, success: "想い出を投稿しました"
     else
+      flash.now[:danger] = "想い出の投稿に失敗しました"
       render :new
     end
   end
@@ -38,15 +39,16 @@ class BoardsController < ApplicationController
 
   def update
     if @board.update(board_params)
-      redirect_to @board
+      redirect_to @board, success: "想い出を更新しました"
     else
+      flash.now[:danger] = "想い出の更新に失敗しました"
       render :edit
     end
   end
 
   def destroy
     @board.destroy!
-    redirect_to boards_path
+    redirect_to boards_path, success: "想い出の記事を削除しました"
   end
 
   private
