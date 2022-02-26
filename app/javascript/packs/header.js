8176b338faba498808cda76cc99167f1102ebe52
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-02-26 16:42:24 +0900

    fix: スマホ時ボタンを押したらメニューが表示されるように修正

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -1,46 +1,8 @@
-function selected() {
-  var targeted = event.target;
-  var clicked = targeted.parentElement;
+window.onload = function(){
+  const button = document.querySelector('#navbar-toggle'); // Hamburger Icon
+  const menu = document.querySelector('#navbar-collapse'); // Menu
 
-  var child = clicked.children;
-  console.log(child);
-
-  for (let i = 0; i < child.length; i++) {
-    if (child[i].classList.contains("text-white")) {
-      console.log(child[i]);
-      child[i].classList.remove("text-white", "bg-indigo-600");
-      child[i].classList.add(
-        "text-gray-600",
-        "bg-gray-50",
-        "border",
-        "border-white"
-      );
-    }
-  }
-
-  targeted.classList.remove(
-    "text-gray-600",
-    "bg-gray-50",
-    "border",
-    "border-white"
-  );
-  targeted.classList.add("text-white", "bg-indigo-600");
-}
-
-function selectNew() {
-  var newL = document.getElementById("list");
-  newL.classList.toggle("hidden");
-
-  document.getElementById("ArrowSVG").classList.toggle("rotate-180");
-}
-
-function selectedSmall() {
-  var text = event.target.innerText;
-  var newL = document.getElementById("list");
-  var newText = document.getElementById("textClicked");
-  newL.classList.add("hidden");
-  document.getElementById("ArrowSVG").classList.toggle("rotate-180");
-  newText.innerText = text;
-
-  document.getElementById("s1").classList.remove("hidden");
-}
+  button.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+};
