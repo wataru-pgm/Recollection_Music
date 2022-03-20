dc2f6a90dcef2d24c7205f43c92abbfba9e87205
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-03-20 18:03:38 +0900

    add: faviconの実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      title: 'Re:Collect',
+      reverse: true,
+      charset: 'utf-8',
+      description: '音楽と思い出を共有するSNSアプリ',
+      keywords: '音楽, 想い出, 共有, Re:Collect, リコレクト',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.png') },
+        { href: image_url('favicon.png'), rel: 'apple-touch-icon', sizes: '180x120', type: 'image/jpg' }
+      ],
+      # og: {
+      #   site_name: 'Re:Collect',
+      #   title: 'Re:Collect',
+      #   description: :description,
+      #   type: 'website',
+      #   url: request.original_url,
+      #   image: image_url('ogp.png'),
+      #   locale: 'ja_JP'
+      # },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@wataru_pgm'
+      }
+    }
+  end
 end
