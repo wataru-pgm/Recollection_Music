f70f5cbdb6d9883473dd5842b9bdf7a81383f3b3
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-05-19 21:48:31 +0900

    add: userのモデルスペック作成

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'バリデーション' do
+    it '名前が入力されていないとき' do
+      user = build(:user, name: "")
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
+
+    it 'メールアドレスが入力されていないとき' do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it 'パスワードが入力されていないとき' do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("は3文字以上で入力してください")
+    end
+
+    it 'パスワード確認が入力されていないとき' do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("を入力してください")
+    end
+
+    it 'メールアドレスが重複している時' do
+      user1 = create(:user, email: "test@example.com")
+      user2 = build(:user, email: "test@example.com")
+      user2.valid?
+      expect(user2.errors[:email]).to include("はすでに存在します")
+    end
+  end
+end
