9a605d82d3c7e6cbc643894e71382ad5f1e1781d
Wataru Morikami <w.morikami.pgm@gmail.com>
2022-05-22 02:02:00 +0900

    fix: コメントのモデルスペック修正

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  it 'コメントが入力されていないとき' do
-    comment = build(:comment, body: "")
+  it 'コメントが4文字以下のときNG' do
+    comment = build(:comment, body: "コメント")
     comment.valid?
-    expect(comment.errors[:body]).to include("を入力してください")
+    expect(comment.errors[:body]).to include("は5文字以上で入力してください")
+  end
+
+  it 'コメントが101文字以上のときNG' do
+    comment = build(:comment, body: "テストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテストほげテ")
+    comment.valid?
+    expect(comment.errors[:body]).to include("は100文字以内で入力してください")
   end
 end
